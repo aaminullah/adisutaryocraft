ca3f0832b6d7f6a7047c03b8bef8fbd0ab90c04e
Ahmad Aminullah <ahmad.aminullah@ui.ac.id>
2023-08-26 19:39:58 +0700

    fix minor

--- src/about/About.js
+++ src/about/About.js
@@ -2,7 +2,6 @@ import './About.css';
 import thgl from './THGL.svg';
 import tgl from './TGL.svg';
 
-
 function About() {
   return (
     <div className="About aboutpage container-fluid">
@@ -18,53 +17,16 @@ function About() {
             <img src={thgl} className="logoAboutASC animate__animated animate__fadeInLeft" alt="logo" />
           </div>
           <div className="col-md-6 kolom2">
-            <p className="story animate__animated animate__fadeInUp">Adi Sutaryo Craft merupakan UMKM yang bergerak di bidang kriya dengan bahan dasar
-              yang digunakan adalah kayu pinus. Didirikan oleh Bapak Adi Sutaryo dan Bapak Siswo 
-              pada tahun 2015 dan bertempat di Dusun Gondang RT02 RW12 Nglebak, Tawangmangu, 
-              Karanganyar, Jawa Tengah. Beragam produk telah dihasilkan mulai dari perabotan rumah,
-              peralatan dapur, ornamen hias, hingga produk <i>custom</i> yang dapat dipesan sesuai
-              permintaan.
+            <p className="story animate__animated animate__fadeInUp">Adi Sutaryo Craft merupakan UMKM 
+              yang bergerak di bidang kriya dengan bahan dasar yang digunakan adalah kayu pinus. 
+              Didirikan oleh Bapak Adi Sutaryo dan Bapak Siswo pada tahun 2015 dan bertempat 
+              di Dusun Gondang RT02 RW12 Nglebak, Tawangmangu, Karanganyar, Jawa Tengah. Beragam produk 
+              telah dihasilkan mulai dari perabotan rumah, peralatan dapur, ornamen hias, 
+              hingga produk yang dapat dipesan sesuai permintaan.
             </p>
           </div>
         </div>
       </div>
-
-      {/* <div className="acont2">
-        <div className="row rowContact">
-          <h1 className="headerContact animate__animated animate__fadeInDown">Hubungi Kami</h1>
-        </div>
-        <div className="row rowAllContact pt-4 d-flex justify-content-center">
-          <div className="col-md-6 kolomC1">
-            <a href="mailto:adisutaryocraft357@gmail.com" target="_blank" rel="noopener noreferrer" className="embtn btn btn-outline-light btn-lg"><i class="fa-regular fa-envelope"></i> adisutaryocraft357@gmail.com</a>
-            <a href="http://wa.me/+6281215805269" target="_blank" rel="noopener noreferrer" className="wabtn btn btn-outline-light btn-lg"><i class="fa-brands fa-whatsapp"></i> +62 812-1580-5269</a>
-          </div>
-          <div className="col-md-6 kolomC1">
-            
-          </div>
-        </div>
-      </div> */}
-      
-
-
-      {/* <div className="cont">
-        <h1 className="headerAbout">Tentang Kami</h1>
-        <img src={tgl} className="imgbg" alt="imgbg"></img>
-        <div className="row">
-          <div className="col">
-            <img src={thgl} className="logoAbout" alt="logo" />
-          </div>
-          <div className="col">
-            <p className="story">Adi Sutaryo Craft merupakan UMKM yang bergerak di bidang kriya dengan bahan dasar
-              yang digunakan adalah kayu pinus. Didirikan oleh Bapak Adi Sutaryo dan Bapak Siswo 
-              pada tahun 2015 dan bertempat di Dusun Gondang RT02 RW12 Nglebak, Tawangmangu, 
-              Karanganyar, Jawa Tengah. Beragam produk telah dihasilkan mulai dari perabotan rumah,
-              peralatan dapur, ornamen hias, hingga produk <i>custom</i> yang dapat dipesan sesuai
-              permintaan.
-            </p>
-          </div>
-        </div>
-      </div> */}
-
     </div>
     
   );
--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,9 +1,38 @@
 import asc from './asc1.svg';
 import './Navbar.css';
 import { Outlet, Link } from "react-router-dom";
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
 
+const translationsEn = {
+    home: "Home",
+    product: "Product",
+    about: "About",
+  };
+  
+  const translationsId = {
+    home: "Beranda",
+    product: "Produk",
+    about: "Tentang Kami",
+  };
+  
+  i18n
+    .use(initReactI18next) // passes i18n down to react-i18next
+    .init({
+      resources: {
+        en: { translation: translationsEn },
+        id: { translation: translationsId },
+      },
+      lng: "en",
+      fallbackLng: "en",
+      interpolation: { escapeValue: false },
+    });
 
 function Navbar() {
+  const { t } = useTranslation();
+  const onChange = (event) => {
+    i18n.changeLanguage(event.target.value);
+  };
   return (
     <div class="Navbar">
         <nav class="navbar navbar-expand-lg fixed-top">
@@ -18,35 +47,23 @@ function Navbar() {
                 <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
                         <Link className="nav-link" to="/" style={{textDecoration: 'none'}}>
-                            Beranda
+                            {t("home")}
                         </Link> 
                         <Link className="nav-link" to="/product" style={{textDecoration: 'none'}}>
-                            Produk
+                            {t("product")}
                         </Link>
-                        <Link className="nav-link" to="/about" style={{textDecoration: 'none'}} data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
-                            Tentang Kami
+                        <Link className="nav-link" to="/about" style={{textDecoration: 'none'}}>
+                            {t("about")}
                         </Link>
+                        <select class="form-select form-select-sm mx-3" aria-label="Small select example" onChange={onChange}>
+                            <option className="option" value="en" selected>EN</option>
+                            <option className="option" value="id">ID</option>
+                        </select>
                     </div>
                 </div>
             </div>
         </nav>
         <Outlet />
-        {/* <header id="header" class="header fixed-top">
-            <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
-                <a href="/" class="logo d-flex align-items-center">
-                    <img src={asc} alt="Logo"/>
-                </a>
-                <nav id="navbar" class="navbar">
-                    <ul>
-                        <li><Link to="/" style={{textDecoration: 'none'}}>Home</Link></li>
-                        <li><Link to="/product" style={{textDecoration: 'none'}}>Product</Link></li>
-                        <li><Link to="/about" style={{textDecoration: 'none'}}>About</Link></li>
-                    </ul>
-                    <i class="bi bi-list mobile-nav-toggle"></i>
-                </nav>
-                <Outlet />
-            </div>
-        </header> */}
     </div>
   );
 }
